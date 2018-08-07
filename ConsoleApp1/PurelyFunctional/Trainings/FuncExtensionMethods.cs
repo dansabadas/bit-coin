8f0914a6ffe31757e4578ea0d42d1c6a4e93cbba
DESKTOP-8OGCHC2\dan_s <dan.sabadas@gmail.com>
2018-08-07 08:18:40 +0300

    arrived at 2.2.2 in COncurrency in .Net book

---
+++ ConsoleApp1/PurelyFunctional/Trainings/FuncExtensionMethods.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ConsoleApp1.PurelyFunctional.Trainings
+{
+    static class FuncExtensionMethods
+    {
+        static Func<A, C> Compose<A, B, C>(this Func<A, B> f, Func<B, C> g)
+          => (n) => g(f(n));
+    }
+}
