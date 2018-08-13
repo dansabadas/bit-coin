1c0947b0fcce3c5d66c488f8745e139fbde8b3b5
DESKTOP-8OGCHC2\dan_s <dan.sabadas@gmail.com>
2018-08-13 22:08:05 +0300

    arrived at chapter 4

--- ConsoleApp1/PurelyFunctional/Trainings/FuncExtensionMethods.cs
+++ ConsoleApp1/PurelyFunctional/Trainings/FuncExtensionMethods.cs
@@ -5,6 +5,43 @@ using System.Threading;
 
 namespace ConsoleApp1.PurelyFunctional.Trainings
 {
+    public sealed class FList<T>
+    {
+        private FList(T head, FList<T> tail)
+        {
+            Head = head;
+            Tail = tail.IsEmpty
+                ? FList<T>.Empty 
+                : tail;
+            IsEmpty = false;
+        }
+
+        private FList()
+        {
+            IsEmpty = true;
+        }
+
+        public T Head { get; }
+
+        public FList<T> Tail { get; }
+
+        public bool IsEmpty { get; }
+
+        public static FList<T> Cons(T head, FList<T> tail)
+        {
+            return tail.IsEmpty
+            ? new FList<T>(head, Empty)
+            : new FList<T>(head, tail);
+        }
+
+        public FList<T> Cons(T element)
+        {
+            return FList<T>.Cons(element, this);
+        }
+
+        public static readonly FList<T> Empty = new FList<T>();
+    }
+
     public sealed class Atom<T> where T : class
     {
         private volatile T _value;
@@ -30,8 +67,15 @@ namespace ConsoleApp1.PurelyFunctional.Trainings
 
     static class FuncExtensionMethods
     {
-        static Func<A, C> Compose<A, B, C>(this Func<A, B> f, Func<B, C> g)
-          => (n) => g(f(n));
+        public static void Run()
+        {
+            FList<int> list1 = FList<int>.Empty;
+            FList<int> list2 = list1.Cons(1).Cons(2).Cons(3);
+            FList<int> list3 = FList<int>.Cons(1, FList<int>.Empty);
+            FList<int> list4 = list2.Cons(2).Cons(3);
+        }
+
+        static Func<A, C> Compose<A, B, C>(this Func<A, B> f, Func<B, C> g) => (n) => g(f(n));
 
         private static readonly Dictionary<Tuple<Type, Type>, object> __cache = new Dictionary<Tuple<Type, Type>, object>();
         public static Func<T, R> Memoize<T, R>(this Func<T, R> func)
