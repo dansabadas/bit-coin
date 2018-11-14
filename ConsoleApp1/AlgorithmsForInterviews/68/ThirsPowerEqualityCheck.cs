abda6671223b679ad2f9285804eb4dbd847c4edb
Dan Sabadis <dsabadis@sdl.com>
2018-11-14 14:56:41 +0200

    added examples from the designated pages

---
+++ ConsoleApp1/AlgorithmsForInterviews/68/ThirsPowerEqualityCheck.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1.Algorithms._68
+{
+    /// <summary>
+    /// Print all positive integer solutions to the equation a3 + b3 = c3 + d3 where a, b, c and d are integers between 1 and 1000.
+    /// </summary>
+    public static class ThirdPowerEqualityCheck
+    {
+        public static void Run()
+        {
+            var cachedSums = new Dictionary<int, List<Tuple<int, int>>>();
+            for (int a = 1; a <= 1000; a++)
+            {
+                for (int b = 1; b <= 1000; b++)
+                {
+                    int result = a * a * a + b * b * b;
+                    var currentTuple = Tuple.Create(a, b);
+                    if (!cachedSums.TryGetValue(result, out var cachedResult))
+                    {
+                        cachedResult = new List<Tuple<int, int>>();
+                        cachedSums.Add(result, cachedResult);
+                    }
+
+                    cachedResult.Add(currentTuple);
+                }
+            }
+
+            //var bigSol = cachedSums.Values.AsEnumerable().Where(list => list.Count > 2).ToList();
+            //Console.WriteLine(Math.Pow(428, 3) + Math.Pow(346, 3));
+            //Console.WriteLine(Math.Pow(492, 3) + Math.Pow(90, 3));
+            //Console.WriteLine(Math.Pow(493, 3) + Math.Pow(11, 3));
+            int currentIteratedSolution = 0;
+            foreach (var keyValuePair in cachedSums)
+            {
+                foreach (var (a, b) in keyValuePair.Value)
+                {
+                    foreach (var (c, d) in keyValuePair.Value)
+                    {
+                        Console.WriteLine($"{++currentIteratedSolution}:({a},{b},{c},{d})");
+                    }
+                }
+            }
+
+            //for (int c = 1; c <= 1000; c++)
+            //{
+            //    for (int d = 1; d <= 1000; d++)
+            //    {
+            //        int result = c * c * c + d * d * d;
+            //        if (cachedSums.TryGetValue(result, out var cachedResult))
+            //        {
+            //            foreach (var (a, b) in cachedResult)
+            //            {
+            //                Console.WriteLine($"{++currentIteratedSolution}:({a},{b},{c},{d})");
+            //            }
+            //        }
+            //    }
+            //}
+        }
+    }
+}
---
+++ ConsoleApp1/AlgorithmsForInterviews/70/StringPermutationsFinder.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1.Algorithms._70
+{
+    /// <summary>
+    /// Given a smaller string a and a bigger string b, design an algorithm to find all permutations
+    /// of the shorter string within the longer one. Print the location of each permutation.
+    /// </summary>
+    public static class StringPermutationsFinder
+    {
+        public static void Run()
+        {
+            string shortstr1 = "abbc";
+            string longsstr2 = "cbabadcbbabbcbabaabccbabc";
+
+            var shortStrCharsStatisticsDictionary = shortstr1
+                .ToCharArray()
+                .GroupBy(c => c)
+                .Select(group => new {Value = group.Key, Count = group.Count()})
+                .ToDictionary(a => a.Value, a => a.Count);
+
+            var shortStrLength = shortstr1.Length;
+            int matches = 0;
+            for (int i = 0; i <= longsstr2.Length - shortStrLength; i++)
+            {
+                var currentlyIteratedShortChunkStatisticsList = longsstr2
+                    .Substring(i, shortStrLength)
+                    .ToCharArray()
+                    .GroupBy(c => c)
+                    .Select(group => new { Value = group.Key, Count = group.Count() })
+                    .ToList();
+
+                if (shortStrCharsStatisticsDictionary.Count != currentlyIteratedShortChunkStatisticsList.Count)
+                {
+                    continue;
+                }
+
+                bool mismatchDetected = false;
+                foreach (var charCount in currentlyIteratedShortChunkStatisticsList)
+                {
+                    if (!shortStrCharsStatisticsDictionary.ContainsKey(charCount.Value))
+                    {
+                        mismatchDetected = true;
+                        break;
+                    }
+
+                    if (shortStrCharsStatisticsDictionary[charCount.Value] != charCount.Count)
+                    {
+                        mismatchDetected = true;
+                        break;
+                    }
+                }
+
+                if (!mismatchDetected)
+                {
+                    matches++;
+                }
+            }
+
+            Console.WriteLine($"matches={matches}");
+        }
+    }
+}
